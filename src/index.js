3749cdb3b555df1e9e9ab5f5e663a14e3e473d46
wesferoli <wesleyfer.oli97@gmail.com>
2020-08-01 16:49:09 -0300

    Home terminada

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+      <Route path="/" component={Home} exact />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/pages/Home/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import PageDefault from '../../components/PageDefault';
+import Menu from '../../components/Menu';
+import Footer from '../../components/Footer';
+
+function Home() {
+  return (
+    <div>
+      <Menu />
+
+      <BannerMain 
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-End?"}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel 
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel 
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
