b804a41ce19029365d6209baecef6bea1f1da8ef
wesferoli <wesleyfer.oli97@gmail.com>
2020-08-01 18:27:29 -0300

    Add git e linkedin links ao footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Github from '../../assets/img/Github-logo.png';
+import Linkedin from '../../assets/img/Linkedin-logo.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://github.com/wesferoli">
+        <img src={Github} alt="Logo Github" />
+      </a>
+      <a href="https://www.linkedin.com/in/wesferoli/">
+        <img src={Linkedin} alt="Logo Linkedin" />
       </a>
       <p>
         Orgulhosamente criado durante a
